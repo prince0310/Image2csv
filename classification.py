15d0c9f9f0af03cd8dd644242b4755ec8785f774
Prince kumar <85225054+prince0310@users.noreply.github.com>
2021-12-14 17:16:56 +0530

    Add files via upload

---
+++ classification.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import numpy as np
+import sys
+import os
+import csv
+import cv2 as cv
+
+def createfilelist(mydir, fotmat = ".jpg" ):
+    filelist = []
+    print(mydir)
+    for root,dirs,files in os.walk(mydir,topdown= False):
+        for name in files :
+            if name.endswith(fotmat):
+                fullName = os.path.join(root,name)
+                filelist.append(fullName)
+        
+    return filelist
+
+myFilelist = createfilelist("/home/prince/dataset/AbNormal")
+
+for file in myFilelist:
+    print(file)
+    img_file = Image.open(file)
+    width,height = img_file.size
+    format = img_file.format
+    mode = img_file.mode
+    img_grey = img_file.convert('L')
+
+    value = np.asarray(img_grey.getdata(),dtype=np.int).reshape((img_grey.size[1],img_grey.size[0]))
+    value = value.flatten()
+    print(value)
+    with open("image_to_csv.csv","a") as f :
+        writer = csv.writer(f)
+        writer.writerow(value)
+
+
+# img  = cv.imread("/home/prince/dataset/AbNormal/HPT_1.jpg")
+# cv.imshow("image",img)
+# cv.waitKey(0)
+# 
